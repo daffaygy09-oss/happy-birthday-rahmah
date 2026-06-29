e54ebb186ac1039268a6cf10d55aa6c24199d2fb
daffaygy09-oss <daffaygy09@gmail.com>
2026-06-29 20:34:41 +0700

    Add initial setup for script.js
    Initialize script.js with DOM element references.

---
+++ script.js
@@ -0,0 +1,10 @@
+const loading = document.getElementById("loading");
+const main = document.getElementById("main");
+const birthdayPage = document.getElementById("birthdayPage");
+const openBtn = document.getElementById("openBtn");
+const music = document.getElementById("music");
+const slide = document.getElementById("slide");
+const typing = document.getElementById("typing");
+
+// Loading 3 detik
+window.onload
